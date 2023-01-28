6e51b1d1fe3acb53430a41dd62d7f90faac4630d
muhannadhmnh <mrmuhannadh@gmail.com>
2023-01-28 16:56:21 +0530

    Started

---
+++ src/Pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Test from '../Components/TopNav/test'
+import TopNav from '../Components/TopNav/TopNav'
+
+const Home = () => {
+  return (
+    <div>
+      <TopNav/>
+      <Test/>
+    </div>
+  )
+}
+
+export default Home
